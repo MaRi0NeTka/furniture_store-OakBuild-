fca6f7d46bb712439b09621c8f4f57096b6c4899
MaRioNeTka <maksnedilko0@gmail.com>
2025-05-20 18:00:23 +0300

    Add-to-cart ajax request and json response

--- carts/templatetags/carts_tag.py
+++ carts/templatetags/carts_tag.py
@@ -1,6 +1,6 @@
 from django import template
 
-from carts.models import Cart
+from carts.utils import get_user_carts
 
 register = template.Library()
 
@@ -8,5 +8,4 @@ register = template.Library()
 def user_carts(request):
     # Получаем все корзины пользователя
     # Чтобы они были доступны в шаблоне
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
+    return get_user_carts(request)
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)#получаем QuerySet по слагу
+def cart_add(request):
+    product_id = request.POST.get('product_id')# получаем id товара из POST запроса
+
+    product = Products.objects.get(id=product_id)#получаем QuerySet по слагу
     if request.user.is_authenticated:#проверяем авторизован ли пользователь
         cart = Cart.objects.filter(user=request.user, product=product)# получаем карточку товара в корзине пользователя
         if cart.exists(): #проверяем существует ли карточка товара в корзине, если существует, то прибавляем к ней +1
@@ -17,13 +22,25 @@ def cart_add(request, product_slug):
         else: #если карточки товара в корзине нет, то создаем новую
             cart = Cart.objects.create(user=request.user, product=product, quantity=1)
     
-    return redirect(request.META['HTTP_REFERER'])# оставляем пользователя на той же странице, где он кликнул на кнопку "Добавить в корзину"
+    user_carts = get_user_carts(request)# получаем все корзины с товарами пользователя
+
+    #передаем данные для перерисовки в шаблоне корзины
+    cart_items_html = render_to_string(
+        'carts/includes/include_cart.html', {'carts':user_carts}, request=request
+    )
+
+    response_data = {
+        'message': 'Товар добавлен в корзину',
+        'cart_items_html':cart_items_html,
+    }
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, cart_id):
-    cart = Cart.objects.get(id=cart_id)
-    cart.delete()
-    return redirect(request.META["HTTP_REFERER"])
+def cart_remove(request):
+    ...
+    # cart = Cart.objects.get(id=cart_id)
+    # cart.delete()
+    # return redirect(request.META["HTTP_REFERER"])
