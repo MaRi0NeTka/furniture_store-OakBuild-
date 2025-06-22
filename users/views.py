ccf4cf2795be811fd925c1a3c5abaaa9598129e8
MaRioNeTka <maksnedilko0@gmail.com>
2025-06-22 14:42:43 +0300

    Small correction

--- users/views.py
+++ users/views.py
@@ -103,6 +103,7 @@ class UserCartView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Корзина'
+        return context
 
 # def login(request):
 #     if request.method == 'POST':
